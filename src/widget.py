6b30d7918d790267ee5006a76191cf6bd83cd0bb
Denis Antropov <de94onisus@gmail.com>
2023-10-29 22:22:56 +0400

    Add function "card_acc_info_mask()" in src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,15 @@
-def func():
-    pass
+from src.masks import get_account_mask, get_card_mask
+
+
+def card_acc_info_mask(card_acc_info: str) -> str:
+    """
+    Функция принимает на вход строку с информацией о типе карты/счета и номер карты/счета
+    Возвращает эту строку с замаскированным номером карты/счета
+    """
+    card_acc_list = card_acc_info.strip().split()
+    if card_acc_list[0] == "Счет":
+        return f"{card_acc_list[0]} {get_account_mask(card_acc_list[1])}"
+    elif card_acc_list[0] == "Visa":
+        return f"{card_acc_list[0]} {card_acc_list[1]} {get_card_mask(card_acc_list[-1])}"
+    else:
+        return f"{card_acc_list[0]} {get_card_mask(card_acc_list[-1])}"
