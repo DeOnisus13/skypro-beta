cb220a37882a811e7dd3358d0304b66a25bfee38
Denis Antropov <de94onisus@gmail.com>
2023-10-30 05:29:40 +0400

    Add function "modify_date" in src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -11,3 +11,14 @@ def card_acc_info_mask(card_acc_info: str) -> str:
         return f"{card_acc_list[0]} {get_account_mask(card_acc_list[1])}"
     else:
         return f'{" ".join(card_acc_list[:-1])} {get_card_mask(card_acc_list[-1])}'
+
+
+def modify_date(date_raw: str) -> str:
+    """
+    Функция для изменения вывода даты
+    :param date_raw: Дата для изменения
+    :return: Отформатированная дата
+    """
+    date_list = date_raw.replace("T", " ").split()
+    change_date_list = date_list[0].split("-")
+    return ".".join(change_date_list[::-1])
