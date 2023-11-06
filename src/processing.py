38bdd89bb66c11d5060ccd38e56b590014a00aa0
Denis Antropov <de94onisus@gmail.com>
2023-11-06 17:58:24 +0400

    Add function "sort_transactions" in src/processing.py

--- src/processing.py
+++ src/processing.py
@@ -4,3 +4,17 @@ def filter_transactions(transactions: list[dict], state: str = "EXECUTED") -> li
     Возвращает новый список словарей, у которых ключ 'state' содержит переданное в функцию значение.
     """
     return [transaction for transaction in transactions if state.lower() == transaction["state"].lower()]
+
+
+def sort_transactions(transactions: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Функция принимает на вход список словарей и возвращает новый список словарей, отсортированный
+    по ключу 'date' (по умолчанию в порядке убывания даты, т.е. от новой к старой). Если нужен порядок по
+    возрастанию (от старой к новой), то нужно указать второй параметр False.
+    """
+    filter_for_sort = lambda transaction: transaction["date"]
+
+    if not reverse:
+        return sorted(transactions, key=filter_for_sort)
+    else:
+        return sorted(transactions, key=filter_for_sort, reverse=True)
